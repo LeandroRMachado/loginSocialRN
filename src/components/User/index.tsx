3f7ad52f8ce29374a6e123b5fd3b70e2425acf49
LeandroRMachado <leandroskullofwar@outlook.com>
2022-04-06 21:18:19 -0300

    last commit

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -2,19 +2,29 @@ import React from 'react';
 
 import { Container, Photo, Name, Email } from './styles';
 
-export function User() {
+export type UserProps = {
+  email: string
+  name: string
+  picture: string
+}
+
+type Props = {
+  user: UserProps
+}
+
+export function User({ user }: Props) {
   return (
     <Container>
       <Photo
-        source={{ uri: "https://github.com/LeandroRMachado.png" }}
+        source={{ uri: user.picture }}
       />
 
       <Name>
-        Leandro
+        {user.name}
       </Name>
 
       <Email>
-        leandromachado@outlook.com
+        {user.email}
       </Email>
     </Container>
   );
